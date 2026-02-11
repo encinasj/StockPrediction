3d380219750ae665f1aaf51530026da1fc6490b9
Encinas <jose.encinas@gmail.com>
2026-02-11 16:39:40 -0700

    create principal commponents

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-
+import './assets/css/style.css'
+import Header from './assets/components/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
